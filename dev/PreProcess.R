62b161de71b7bb279ef281e3f2d033ef2afe9608
Hyunjin (Jin) Song <hyunjin.song@univie.ac.at>
2018-12-14 13:20:29 +0100

    initial commit

---
+++ dev/PreProcess.R
@@ -0,0 +1,10 @@
+
+
+
+
+## required packages
+require(haven)
+require(data.table)
+
+## load data
+dat <- read_csv("/Users/songh95/Dropbox/(19) 2018 Summer/CR_2018/Dat/DiscussionForumThreeWavePanel(N=341).sav")
